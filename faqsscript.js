5dc26bf0d445726c0e6e5bb71db84372392ec20a
oliotodo <46330007+oliotodo@users.noreply.github.com>
2019-09-15 20:39:42 -0400

    Accordion

--- faqsscript.js
+++ faqsscript.js
@@ -1,8 +1,14 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
