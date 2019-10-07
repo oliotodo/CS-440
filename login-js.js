ece0d26e1a6204b95c1587b3da46530900de7eb3
oliotodo <46330007+oliotodo@users.noreply.github.com>
2019-10-07 00:21:41 -0400

    Internship Example

--- login-js.js
+++ login-js.js
@@ -1,9 +1,3 @@
-function ValidateEmail(mail)
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
+function myFunction() {
+  alert("Please enter your Bellarmine University email and password first.");
 }
