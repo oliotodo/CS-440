216bfd9149b704631f4846dd913f8e63babcf5be
oliotodo <46330007+oliotodo@users.noreply.github.com>
2019-12-08 19:33:52 -0500

    Internship Table

---
+++ internships-js.js
@@ -0,0 +1,10 @@
+var onImg= "heart.png";
+var offImg= "redheart.png";
+
+function toggle(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+}
